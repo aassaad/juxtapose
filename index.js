05111ca650c3bd6911901abb56a8eb1e8ba29b01
aassaad <alec.assaad@gmail.com>
2017-04-02 18:17:35 -0400

    make export and window vars consistent

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", {value: true});
 var _j = require('./juxtapose/js/juxtapose');
-exports.Juxtapose = _j;
+exports.juxtapose = _j;
 exports.JXSlider = _j.JXSlider;
